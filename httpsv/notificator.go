c2b33b60616cbf991d4462358224c3d97d42db0e
walkure <walkure@3pf.jp>
2022-02-23 00:32:25 +0900

    ping message mroe sparseness

--- httpsv/notificator.go
+++ httpsv/notificator.go
@@ -27,7 +27,7 @@ const (
 	writeWait = 1 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
-	pongWait = 2 * time.Second
+	pongWait = 60 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
